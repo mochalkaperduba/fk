8380a79ffa5664d970d22d9a1dbd8416808f4264
mochalkaperduba <125117054+mochalkaperduba@users.noreply.github.com>
2023-02-11 16:37:11 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
 import undetected_chromedriver as chr
+
+def login(d,u,p):
+  d.find_element_by_id("loginForLoginFom").send_keys(u)
+  d.find_element_by_id("passwordForLoginForm").send_keys(p)
+  d.find_element_by_name("do_login").click()
+
+
+
 d = chr.Chrome(version_main = 109)
 d.get("https://ficbook.net")
 print(repr(d))
